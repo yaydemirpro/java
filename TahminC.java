856570ff104e5c690ef8c022b96ef41d754d171a
Yusuf A <99358815+yaydemirpro@users.noreply.github.com>
2022-11-14 14:04:16 +0100

    Guess a number
    Guess a random number and give message to user for guessing a new number lower or bigger. When number is found, give message of congrats and how many attempt were maken.

---
+++ TahminC.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class TahminC {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+		int r1 =r.nextInt(20);
+		System.out.println(r1);
+		int counter = 1;
+		while (true) {
+			String guess = JOptionPane.showInputDialog("Guess a number");
+			int t = Integer.parseInt(guess);
+			if(t==r1) {
+				JOptionPane.showMessageDialog(null, "Congratulations!");
+				JOptionPane.showMessageDialog(null, "Congratulations! You found the number in "+counter+ " right(s)");
+				break;
+			}
+			else if (r1<t) {
+				JOptionPane.showMessageDialog(null, "Guess a smaller number");
+				counter++;
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "Guess a bigger number");
+				counter++;
+			}
+		}
+
+	}
+
+}
